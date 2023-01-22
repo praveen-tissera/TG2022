97c77a4eac407769358ac744654202dd4efbb8d9
praveen-tissera <praveen.tissera@gmail.com>
2023-01-22 14:00:44 +0530

    db change

---
+++ seekers_db.sql
@@ -0,0 +1,257 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 22, 2023 at 09:29 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `seekers_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking_details`
+--
+
+CREATE TABLE `booking_details` (
+  `booking_id` int(11) NOT NULL,
+  `slot_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  `contact_number` varchar(15) NOT NULL,
+  `staus` enum('active','inactive') NOT NULL,
+  `created_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `booking_details`
+--
+
+INSERT INTO `booking_details` (`booking_id`, `slot_id`, `name`, `age`, `contact_number`, `staus`, `created_date`) VALUES
+(21, 2, 'Chamithu Mapalagama', 44, '0775012624', 'active', '2022-12-18'),
+(23, 3, 'Senul Mendis', 44, '0775012624', 'active', '2022-12-18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors_detail`
+--
+
+CREATE TABLE `doctors_detail` (
+  `doctor_id` int(11) NOT NULL,
+  `doctor_name` text NOT NULL,
+  `status` enum('active','inactive') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctors_detail`
+--
+
+INSERT INTO `doctors_detail` (`doctor_id`, `doctor_name`, `status`) VALUES
+(1, 'manelka sumanathilaka', 'active'),
+(2, 'ruwan perera', 'active'),
+(3, 'gayan perera', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor_slots`
+--
+
+CREATE TABLE `doctor_slots` (
+  `slot_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(20) NOT NULL,
+  `status` enum('available','not available') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctor_slots`
+--
+
+INSERT INTO `doctor_slots` (`slot_id`, `doctor_id`, `date`, `time`, `status`) VALUES
+(2, 1, '2022-11-21', '13.00-16.00', 'available'),
+(3, 1, '2022-11-21', '8.00-10.00', 'not available'),
+(4, 3, '2022-11-22', '9.00-12.00', 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issues`
+--
+
+CREATE TABLE `issues` (
+  `issue_id` int(11) NOT NULL,
+  `publication_id` int(11) NOT NULL,
+  `issue_number` int(11) NOT NULL,
+  `issue_date_publication` date NOT NULL,
+  `issue_cover` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `issues`
+--
+
+INSERT INTO `issues` (`issue_id`, `publication_id`, `issue_number`, `issue_date_publication`, `issue_cover`) VALUES
+(1, 1, 2, '2013-02-09', NULL),
+(2, 3, 2, '2013-02-01', NULL),
+(3, 4, 2, '2013-02-01', NULL),
+(4, 5, 2, '2013-02-01', NULL),
+(5, 6, 1, '2015-10-22', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publications`
+--
+
+CREATE TABLE `publications` (
+  `publication_id` int(11) NOT NULL,
+  `publication_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `publications`
+--
+
+INSERT INTO `publications` (`publication_id`, `publication_name`) VALUES
+(1, 'Sandy Shore'),
+(2, 'bloomsbary'),
+(3, 'larry books'),
+(4, 'Nevoda Publishers'),
+(5, 'newTimes books'),
+(6, 'heloos');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_list`
+--
+
+CREATE TABLE `task_list` (
+  `task_id` int(11) NOT NULL,
+  `task_description` varchar(255) NOT NULL,
+  `status` enum('new','assigned','in progress','complete') NOT NULL,
+  `created_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `task_list`
+--
+
+INSERT INTO `task_list` (`task_id`, `task_description`, `status`, `created_date`) VALUES
+(3, 'test task', 'new', '2023-01-08'),
+(4, 'test task', 'new', '2023-01-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login`
+--
+
+CREATE TABLE `user_login` (
+  `id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_login`
+--
+
+INSERT INTO `user_login` (`id`, `user_name`, `user_email`, `user_password`) VALUES
+(1, 'praveen', 'praveen.tissera@gmail.com', '123456'),
+(2, 'sadun', 'sadun@gmail.com', '123456'),
+(3, 'nuwan', 'nuwan@gmail.com', '123456'),
+(4, 'sadun1234', 'sadun123@gmail.com', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking_details`
+--
+ALTER TABLE `booking_details`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD UNIQUE KEY `slot_id` (`slot_id`);
+
+--
+-- Indexes for table `doctors_detail`
+--
+ALTER TABLE `doctors_detail`
+  ADD PRIMARY KEY (`doctor_id`);
+
+--
+-- Indexes for table `doctor_slots`
+--
+ALTER TABLE `doctor_slots`
+  ADD PRIMARY KEY (`slot_id`),
+  ADD KEY `doctor_id` (`doctor_id`);
+
+--
+-- Indexes for table `issues`
+--
+ALTER TABLE `issues`
+  ADD PRIMARY KEY (`issue_id`);
+
+--
+-- Indexes for table `publications`
+--
+ALTER TABLE `publications`
+  ADD PRIMARY KEY (`publication_id`);
+
+--
+-- Indexes for table `task_list`
+--
+ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking_details`
+--
+ALTER TABLE `booking_details`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `doctors_detail`
+--
+ALTER TABLE `doctors_detail`
+  MODIFY `doctor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `doctor_slots`
+--
+ALTER TABLE `doctor_slots`
+  MODIFY `slot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
