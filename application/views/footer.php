92a76c4311e01085417843d50aa3189575103ed7
mnazmaan <mnazmaan@gmail.com>
2023-01-22 13:18:16 +0530

    Copyright version changed

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Amaans Cellular (Pvt) Ltd. 2015</p>
+                    <p>Copyright &copy; Amaans Cellular (Pvt) Ltd. 2022</p>
                 </div>
             </div>
             <!-- /.row -->
