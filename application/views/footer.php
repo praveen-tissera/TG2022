34a410369521fcc84dd2575a5570beeabfdb8e0d
praveen-tissera <praveen.tissera@gmail.com>
2023-01-22 12:57:58 +0530

    copyright version changed

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Mount View Parking (PVT) 2015</p>
+                    <p>Copyright &copy; Mount View Parking (PVT) 2022</p>
                 </div>
             </div>
             <!-- /.row -->
