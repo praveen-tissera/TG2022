c867029170f0a84970ce24c93fe87d2645ba3e87
0710599566 <ranminijayawardena@gmail.com>
2023-01-22 13:23:17 +0530

    copyright version changed

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Supermarket Management System 2015</p>
+                    <p>Copyright &copy; Supermarket Management System 2022</p>
                 </div>
             </div>
             <!-- /.row -->
