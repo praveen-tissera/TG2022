ca8d1f63759c7cfc526e598086dc07b018f394b2
fathahrizwan <abdulfathahrizwan@gmail.com>
2023-01-22 13:18:17 +0530

    copyright version change

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Fathah Engineering (Pvt) Ltd 2023</p>
+                    <p>Copyright &copy; Fathah Engineering (Pvt) Ltd 2022</p>
                 </div>
             </div>
             <!-- /.row -->
