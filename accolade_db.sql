6625c35b33cd0fdcbf696fc41cd4a8599252bc18
KaluDineth <dineth.s.kaluarachchi@gmail.com>
2023-01-29 14:35:17 +0530

    changed databse

--- accolade_db.sql
+++ accolade_db.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 11, 2015 at 06:53 PM
--- Server version: 5.6.14
--- PHP Version: 5.5.6
+-- Generation Time: Oct 23, 2022 at 10:13 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `cidb`
@@ -26,14 +27,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `issues`
 --
 
-CREATE TABLE IF NOT EXISTS `issues` (
-  `issue_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `issues` (
+  `issue_id` int(11) NOT NULL,
   `publication_id` int(11) NOT NULL,
   `issue_number` int(11) NOT NULL,
   `issue_date_publication` date NOT NULL,
-  `issue_cover` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`issue_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `issue_cover` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `issues`
@@ -52,11 +52,10 @@ INSERT INTO `issues` (`issue_id`, `publication_id`, `issue_number`, `issue_date_
 -- Table structure for table `publications`
 --
 
-CREATE TABLE IF NOT EXISTS `publications` (
-  `publication_id` int(11) NOT NULL AUTO_INCREMENT,
-  `publication_name` text NOT NULL,
-  PRIMARY KEY (`publication_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+CREATE TABLE `publications` (
+  `publication_id` int(11) NOT NULL,
+  `publication_name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `publications`
@@ -76,20 +75,67 @@ INSERT INTO `publications` (`publication_id`, `publication_name`) VALUES
 -- Table structure for table `user_login`
 --
 
-CREATE TABLE IF NOT EXISTS `user_login` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user_login` (
+  `id` int(11) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `user_email` varchar(255) NOT NULL,
-  `user_password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `user_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_login`
 --
 
 INSERT INTO `user_login` (`id`, `user_name`, `user_email`, `user_password`) VALUES
-(1, 'praven', 'praveen.tissera@gmail.com', 'srilanka');
+(1, 'praveen', 'praveen.tissera@gmail.com', 'srilanka'),
+(2, 'Caspean', 'caspean@gmail.com', 'pierce'),
+(4, 'Lilith', 'Lilith@gmail.com', 'bloodmoon'),
+(5, 'Maeve', 'Maeve@gmail.com', 'kitten');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `issues`
+--
+ALTER TABLE `issues`
+  ADD PRIMARY KEY (`issue_id`);
+
+--
+-- Indexes for table `publications`
+--
+ALTER TABLE `publications`
+  ADD PRIMARY KEY (`publication_id`);
+
+--
+-- Indexes for table `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `issues`
+--
+ALTER TABLE `issues`
+  MODIFY `issue_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `publications`
+--
+ALTER TABLE `publications`
+  MODIFY `publication_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user_login`
+--
+ALTER TABLE `user_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
